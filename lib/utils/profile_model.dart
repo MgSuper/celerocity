5e6720d8aab94d589e4e244683a21610b7ebdada
KhinMyintMyatThu <kmmt96@gmail.com>
2022-02-17 21:27:26 +0630

    data preparation

---
+++ lib/utils/profile_model.dart
@@ -0,0 +1,24 @@
+class Profile {
+  Profile(
+      {this.name,
+      this.fullname,
+      this.summary,
+      this.birthdate,
+      this.bloodType,
+      this.height,
+      this.native,
+      this.image,
+      this.instaURL,
+      this.facebookURL});
+
+  final String name;
+  final String fullname;
+  final String summary;
+  final String birthdate;
+  final String bloodType;
+  final String height;
+  final String native;
+  final String image;
+  final String instaURL;
+  final String facebookURL;
+}
